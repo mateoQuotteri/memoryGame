05c6a6a50057c66818fc9eb06810c1f2e7d5bd8c
Mateo Quotteri <quotterimateo@gmail.com>
2023-06-13 19:09:06 -0300

    fixing errors of last commit

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
-const allFrontFaces = document.querySelectorAll(".font-face")
+const allFrontFaces = document.querySelectorAll(".front-face")
 const allBackFaces = document.querySelectorAll(".back-face")
 window.onload = function (e)
  {
     setTimeout(() => {
         allFrontFaces.forEach((frontFace) => {
-            frontFace.classList.remove("front-face");
-            frontFace.classList.add("back-face");
+            frontFace.classList.add("opacity")
           });
-      
           allBackFaces.forEach((backFace) => {
-            backFace.classList.add("front-face");
-            backFace.classList.remove("back-face");
+            backFace.classList.remove("back-face")
+
+            backFace.classList.add("front-face")
           });
+          
     }, 2000);
 }
