a2428f4417351b542fd38ef2374c721c8dcad9db
Mateo Quotteri <quotterimateo@gmail.com>
2023-06-15 12:20:10 -0300

    comparing cards selected

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const allFrontFaces = document.querySelectorAll(".front-face")
+const allMemoryCards = document.querySelectorAll(".memory-card")
 const allBackFaces = document.querySelectorAll(".back-face")
 window.onload = (e) =>  
  {
@@ -13,14 +14,43 @@ window.onload = (e) =>
           });
           
     }, 2000);
+
 }
-allBackFaces.forEach((backFace) => {
-  backFace.addEventListener("click" , (e)=> {
-    let broElement = backFace.previousElementSibling;
-    console.log(broElement);
-    backFace.classList.add("back-face")
+
+function comparing(cardOne , cardTwo) {
+  if (cardOne === cardTwo) {
+    console.log("Ganaste");
+  } else {
+    console.log("perdiste");
+  }
+}
+
+let contador = 0;
+let array = []
+allMemoryCards.forEach((memoryCard) => {
+  memoryCard.addEventListener("click" , (e)=> {
+
+    if (contador === 2) {
+      return
+    }
+    let frontFace = memoryCard.querySelector(".front-face");
+
+    frontFace.classList.remove("opacity");
+
+    let cardSelected = frontFace.id;
+
+    let broElement = frontFace.nextElementSibling;
+    broElement.classList.add("back-face")
+    broElement.classList.remove("front-face")
+
+    contador += 1;
+    array.push(cardSelected);
+    if (contador === 2) {
+      let firstCard = array[0];
+      let secondCard = array[1]
+
+      comparing(firstCard, secondCard)
+    }
     
-    backFace.classList.remove("front-face")
-    broElement.classList.remove("opacity")
   })
 });
